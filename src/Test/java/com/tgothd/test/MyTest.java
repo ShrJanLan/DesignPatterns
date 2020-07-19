7e8dc26c72c8d5262d24c252b96d5505ae445e02
ShrJanLan <1606747757@qq.com>
2020-07-19 23:42:29 +0800

    工厂模式

--- src/Test/java/com/tgothd/test/MyTest.java
+++ src/Test/java/com/tgothd/test/MyTest.java
@@ -1,6 +1,7 @@
 package Test.java.com.tgothd.test;
 
 import main.java.com.tgothd.demo.RealizeType;
+import main.java.com.tgothd.factory.PizzaStore2;
 import main.java.com.tgothd.simpleFactory.PizzaSimpleFactory;
 import main.java.com.tgothd.simpleFactory.PizzaStore;
 import org.junit.Test;
@@ -13,18 +14,26 @@ import org.junit.Test;
 public class MyTest extends Object {
 
     @Test
-    public void realizeType() throws CloneNotSupportedException {
+    public void testRealizeType() throws CloneNotSupportedException {
         RealizeType obj1 = new RealizeType();
         RealizeType obj2 = (RealizeType)obj1.clone();
         System.out.println("obj1==obj2?"+(obj1==obj2));
     }
 
     @Test
-    public void simpleFactory(){
+    public void testSimpleFactory(){
         PizzaSimpleFactory pizzaSimpleFactory = new PizzaSimpleFactory();
         PizzaStore pizzaStore = new PizzaStore(pizzaSimpleFactory);
         pizzaStore.orderPizza("vegetables");
         pizzaStore.orderPizza("meat");
+    }
+
+    @Test
+    public void testFactory(){
+        main.java.com.tgothd.factory.PizzaStore pizzaStore = new main.java.com.tgothd.factory.PizzaStore();
+        pizzaStore.orderPizza("vegetables");
+        PizzaStore2 pizzaStore2 = new PizzaStore2();
+        pizzaStore2.orderPizza("meat");
     }
 
 }
